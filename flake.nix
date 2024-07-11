2cac060e3b37c321192c1130c429e54b6120e98d
Diogo Correia <dv_correia@hotmail.com>
2024-07-11 23:20:39 +0100

    Flake init and editorconfig

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "Healthcare Open Source software packages and services for Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: { };
+}
