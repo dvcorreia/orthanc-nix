a62f31dc86dd84cfdb538d1fbb1b418c81741183
Diogo Correia <dv_correia@hotmail.com>
2024-07-29 00:35:22 +0100

    Advancements in the build process (still not working)

--- pkgs/orthanc/orthanc.nix
+++ pkgs/orthanc/orthanc.nix
@@ -1,11 +1,17 @@
 {
   lib,
+  pkgs,
   stdenv,
   fetchhg,
-  xcode,
-  gnumake
 }:
-
+let
+  # trying to get dicom dict setup (not working)
+  dcmtk-w-dict = pkgs.dcmtk.overrideAttrs {
+    cmakeFlags = [
+      "-DDCMTK_DEFAULT_DICT=builtin"
+    ];
+  };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "orthanc";
   version = "1.12.4";
@@ -16,10 +22,59 @@ stdenv.mkDerivation (finalAttrs: {
     sha256 = "16mgii4iki1024sd5fih2s9dwmxfxfivzqdawbwgl2s8pfgisgxf";
   };
 
-  buildInputs = [
-    xcode
+  nativeBuildInputs = with pkgs; [
     gnumake
+    cmake
+    python3
+    curl
+    gtest
+    protobuf
   ];
+
+  buildInputs = with pkgs; [
+    libgcc
+    unzip
+    sqlite
+    openssl
+    civetweb
+    libjpeg
+    libpng
+    lua
+    pugixml
+    jsoncpp
+    libuuid
+    boost
+    #dcmtk-w-dict
+    dcmtk # implements the dicom standard
+    #sqlitecpp
+    #gflags
+    #locale
+  ];
+
+  cmakeFlags = [
+    "-DALLOW_DOWNLOADS=OFF"
+    "-DSTATIC_BUILD=OFF"
+    "-DCMAKE_BUILD_TYPE=Debug"
+  ];
+
+  configurePhase = ''
+    mkdir Build
+    cd ./Build
+    cmake ../OrthancServer/
+  '';
+
+  buildPhase = ''
+    make
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp * $out/bin
+
+    runHook postInstall
+  '';
 
   meta = {
     description = "Orthanc is a lightweight DICOM server for medical imaging";
@@ -30,7 +85,7 @@ stdenv.mkDerivation (finalAttrs: {
       "Alain Mazy"
     ];
     platforms = [
-      "aarch64-darwin"
+      "x86_64-linux"
     ];
   };
 })
