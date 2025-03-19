58ae99551316025c0c9b1b72ea661c240f780325
Diogo Correia <dv_correia@hotmail.com>
2025-03-19 00:27:14 +0000

    work on trying to build the dicomweb plugin

--- plugins/dicomweb.nix
+++ plugins/dicomweb.nix
@@ -3,9 +3,16 @@
   pkgs,
   stdenv,
   fetchhg,
-  orthanc,
+  fetchurl,
+  orthanc-sdk,
 }:
 
+let
+  bootstrap-file = fetchurl {
+    url = "https://orthanc.uclouvain.be/downloads/third-party-downloads/bootstrap-5.3.3.zip";
+    sha256 = "sha256-VdfxznlQQK+4MR3wnSnQ00ZIQAweqrstCi7SIWs9sF0=";
+  };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "orthanc-dicomweb";
   version = "1.18";
@@ -14,25 +21,34 @@ stdenv.mkDerivation (finalAttrs: {
     url = "https://orthanc.uclouvain.be/hg/orthanc-dicomweb/";
     rev = "9535bcd7fa8b";
     sha256 = "sha256-ee271Fcu8yi1gZpTWrCuqhsBdFcPR/JK/fsnJg8PwIc=";
-
   };
 
   nativeBuildInputs = with pkgs; [
     cmake
+    python3
+    unzip
+    gtest
   ];
 
   buildInputs = with pkgs; [
-    orthanc
+    orthanc-sdk
+    jsoncpp
+    boost
   ];
 
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE=Release"
     "-DALLOW_DOWNLOADS=OFF"
     "-DSTATIC_BUILD=OFF"
+    "-DORTHANC_FRAMEWORK_SOURCE=system"
+    "-DORTHANC_FRAMEWORK_ROOT=${orthanc-sdk}/include/orthanc-sdk"
+    "-DORTHANC_FRAMEWORK_LIBRARIES=${orthanc-sdk}/lib/libOrthancFramework.so"
   ];
 
   configurePhase = ''
-    mkdir Build
+    mkdir -p Build/Resources/ThirdParty
+    cp ${bootstrap-file} Build/Resources/ThirdParty/bootstrap-5.3.3.zip
+
     cd ./Build
     cmake .. $cmakeFlags
   '';
